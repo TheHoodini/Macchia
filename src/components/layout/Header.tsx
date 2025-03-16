9ba2af60d6156a7dd03baa18771bb48275ca8526
Juan Diego García <salasjuan@uninorte.edu.co>
2025-03-16 16:59:11 -0500

    style: change fonts

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 export function Header(){
     return(
         <header className="h-20 z-10 flex items-center justify-center w-full border-black border-b-[3px]">
-            <h1 className="font-bold text-4xl">Macchia</h1>
+            <h1 className="font-black text-4xl font-playfair">Macchia</h1>
         </header>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Lato', 'ui-sans-serif', 'system-ui'], 
+        playfair: ['Playfair Display', 'serif'],
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
