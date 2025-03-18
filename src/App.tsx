ab90389bdfc35744f6f20eddbbd1656d8bed13a7
Juan Diego García Salas <salasjuan@uninorte.edu.co>
2025-03-18 20:18:55 +0000

    delete useEffect from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { Header } from "./components/layout/Header";
 import { Navbar } from "./components/layout/Navbar";
 import { MainContent } from "./components/layout/MainContent";
